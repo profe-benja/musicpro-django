19bf94f6d2d31d6625e8c1101c179bca20d6cf87
bemtorres <114621426+profe-benja@users.noreply.github.com>
2023-06-22 03:02:10 -0400

    modelo

--- tienda/models.py
+++ tienda/models.py
@@ -1,3 +1,41 @@
 from django.db import models
 
 # Create your models here.
+
+class Usuario(models.Model):
+    nombre = models.CharField(max_length=100)
+    email = models.EmailField()
+    password = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.nombre 
+
+class Categoria(models.Model):
+    nombre = models.CharField(max_length=100)
+    descripcion = models.TextField()
+    
+    def __str__(self):
+        return self.nombre
+
+class Producto(models.Model):
+    nombre = models.CharField(max_length=100)
+    precio = models.FloatField()
+    categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE)
+    
+class Cliente(models.Model):
+    nombre = models.CharField(max_length=100)
+    email = models.EmailField()
+    password = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.nombre 
+    
+class Boleta(models.Model):
+    fecha = models.DateField()
+    total = models.FloatField()
+    
+class DetalleBoleta(models.Model):
+    cantidad = models.IntegerField()
+    precio = models.FloatField()
+    producto = models.ForeignKey(Producto, on_delete=models.CASCADE)
+    boleta = models.ForeignKey(Boleta, on_delete=models.CASCADE)
